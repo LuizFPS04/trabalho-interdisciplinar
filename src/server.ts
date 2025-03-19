ac491a42d1393d3eb98ba0b6677d4a647a278e02
Luiz Felipe Pereira de Sena <luiz.sena@upflow.me>
2025-03-19 20:30:06 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import app from './app'
 
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+    res.send(`BioGenius API is running...`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
