d0ef8c74a1f5acc712377d54bb593b401414cc2d
Luiz Felipe Pereira de Sena <luiz.sena@upflow.me>
2025-03-19 21:26:48 -0300

    feat: added swagger

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import { swaggerDocs } from './utils/swagger';
+import swaggerUi from 'swagger-ui-express';
+
 import routes from './routes/index';
 
 dotenv.config();
@@ -14,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/api", routes);
 
